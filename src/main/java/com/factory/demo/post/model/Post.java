8b9900c4c79dda4ad2f5c19f7fe1ca66a5d99ee3
yongjun <yongjun@sk.com>
2023-05-19 09:58:24 +0900

    DB Access added

---
+++ src/main/java/com/factory/demo/post/model/Post.java
@@ -0,0 +1,21 @@
+package com.factory.demo.post.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Data
+public class Post {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private Author author;
+}
