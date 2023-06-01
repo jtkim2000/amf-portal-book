4e24a9ec44de20eaba5654769e22d883b6622638
Yongjun <yongjun@sk.com>
2023-06-01 17:10:28 +0900

    api spec chaged

--- src/main/java/com/factory/demo/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/factory/demo/member/service/impl/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package com.factory.demo.member.service.impl;
 
 import com.factory.demo.member.dto.MemberCreateRequestDto;
 import com.factory.demo.member.dto.MemberLoginRequestDto;
+import com.factory.demo.member.dto.LoginResultDto;
+import com.factory.demo.member.dto.LoginSuccessResultDto;
 import com.factory.demo.member.model.Member;
 import com.factory.demo.member.repository.MemberRepository;
 import com.factory.demo.member.service.MemberService;
@@ -18,18 +20,24 @@ public class MemberServiceImpl implements MemberService {
     @Override
     @Transactional
     public Long save(MemberCreateRequestDto requestDto) {
+        // 이미 등록된 Email이면 -1을 return하는 것이 API 스펙이라고 가정함
+        Member savedMember = memberRepository.findByEmail(requestDto.getEmail()); // 혹은 Exists 함수를 사용할 수 있음
+        if (savedMember != null) {
+            return -1L;
+        }
+
         return this.memberRepository.save(requestDto.toEntity()).getId();
     }
 
     @Override
-    public Boolean login(MemberLoginRequestDto requestDto) {
+    public LoginResultDto login(MemberLoginRequestDto requestDto) {
         Member savedMember = this.memberRepository.findByEmail(requestDto.getEmail());
         if (savedMember == null) {
-            return false; // 요구사항에 따라 다른 방법으로 구현할 수 있음
+            return new LoginResultDto(false); // 요구사항에 따라 다른 방법으로 구현할 수 있음
         }
         if (!savedMember.getPassword().equals(requestDto.getPassword())) {
-            return false; // 요구사항에 따라 다른 방법으로 구현할 수 있음
+            return new LoginResultDto(false); // 요구사항에 따라 다른 방법으로 구현할 수 있음
         }
-        return true;
+        return new LoginSuccessResultDto(savedMember.getId(), savedMember.getName(), savedMember.getEmail());
     }
 }
